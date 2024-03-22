87e8e7e0fb90e41cb5b9ce1a5c240d91a1b017c2
rendani99 <rendania460@gmail.com>
2024-03-22 03:42:52 +0200

    fixed code...usestate was inside function buttion.. code was not running

--- project_pro/src/App.jsx
+++ project_pro/src/App.jsx
@@ -1,9 +1,15 @@
+import { useState } from "react";
+
 export const App = () => {
   const btnClick = () => {
+    const [initialValue, setInitialValue] = useState(0);
     console.log("Clicked");
+    setInitialValue(initialValue + 1);
+    console.log(initialValue);
   };
   return (
     <div>
+      {initialValue}
       <button onClick={btnClick}>clickMe</button>
     </div>
   );
